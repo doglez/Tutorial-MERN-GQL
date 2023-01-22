5c5521ce55d7a44f5479943773052d5f42172d66
Danilo Gonzalez Ardon <danilo.j.gonzalez@gmail.com>
2023-01-21 22:35:47 -0600

    fixed strictQuery error

--- server/src/db/dbConnect.ts
+++ server/src/db/dbConnect.ts
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import { MONGO_URL } from "../config/Config";
 
 const dbConnect = async () => {
+    mongoose.set("strictQuery", false);
     const conn: typeof mongoose = await mongoose.connect(MONGO_URL);
 
     console.log(`MongoDB connected: ${conn.connection.host}`.green);
