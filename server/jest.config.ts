11781516d050234322912e49c4971ed1b021831a
Danilo Gonzalez Ardon <danilo.j.gonzalez@gmail.com>
2023-01-31 21:03:55 -0600

    jest config file

---
+++ server/jest.config.ts
@@ -0,0 +1,35 @@
+import { Config } from "jest";
+
+const jestConfig: Config = {
+    clearMocks: true,
+    collectCoverage: true,
+    collectCoverageFrom: [
+        "src/**/*.ts",
+        "src/**/__tests__/**",
+        "!**/index.{ts,js,tsx,jsx}",
+    ],
+    coverageDirectory: "coverage",
+    coverageProvider: "v8",
+    coverageReporters: ["lcov", "text", "text-summary", "html"],
+    coverageThreshold: {
+        global: {
+            branches: 90,
+            functions: 90,
+            lines: 90,
+            statements: 90,
+        },
+    },
+    moduleNameMapper: {
+        "^src/(.*)$": "<rootDir>/src/$1",
+    },
+    preset: "ts-jest",
+    roots: ["./src"],
+    testEnvironment: "node",
+    testMatch: [
+        "**/__tests__/**/*.[jt]s?(x)",
+        "**/?(*.)+(spec|test).[jt]s?(x)",
+    ],
+    transformIgnorePatterns: ["\\\\node_modules\\\\", "\\.pnp\\.[^\\\\]+$"],
+};
+
+export default jestConfig;
